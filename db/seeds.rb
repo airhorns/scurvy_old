64dbc97e24fdeba3c4f4baf8d030281d4cd231b8
(no author) <(no author)@5ef84fe0-a5a3-4991-970f-80cd94fc32a8>
2009-11-06 22:19:22 +0000

    Update to release model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 User.create(:login => "hornairs", "password" => "donthack", "password_confirmation"=>"donthack")
+ReleaseType.create(:type => "DVD Rip", :description => "DVD Quality video ripped from a retail DVD. Best standard definition release type available.", :applies_to => "movie")
+ReleaseType.create(:type => "DVD Screener", :description => "DVD Quality video ripped from a screener DVD. May have black and white scenes, overlay text, or other piracy prevention features. See release notes.", :applies_to => "movie")
